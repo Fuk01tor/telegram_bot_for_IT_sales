39a88113c31c7b1df496d67e03a9d841c517a52f
Sajjad <sajjad.hosseini@futurice.com>
2017-03-12 23:36:18 +0200

    add 3 and 4 option reply markup

--- markup_utils.py
+++ markup_utils.py
@@ -7,3 +7,23 @@ def two_option_markup(first_option, second_option):
     itembtn2 = types.KeyboardButton(second_option)
     markup.add(itembtn1, itembtn2)
     return markup
+
+
+def three_option_markup(first_option, second_option, third_option):
+    markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+    itembtn1 = types.KeyboardButton(first_option)
+    itembtn2 = types.KeyboardButton(second_option)
+    itembtn3 = types.KeyboardButton(third_option)
+    markup.add(itembtn1, itembtn2, itembtn3)
+    return markup
+
+
+def four_option_markup(first_option, second_option,
+                       third_option, fourth_option):
+    markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+    itembtn1 = types.KeyboardButton(first_option)
+    itembtn2 = types.KeyboardButton(second_option)
+    itembtn3 = types.KeyboardButton(third_option)
+    itembtn4 = types.KeyboardButton(fourth_option)
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    return markup
