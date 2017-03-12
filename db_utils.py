0faae223bd2a1b262fe7f418254ff1c9e4a734a8
Sajjad <sajjad.hosseini@futurice.com>
2017-03-12 23:37:59 +0200

    add remove+add+get operations for Monitors

--- db_utils.py
+++ db_utils.py
@@ -13,4 +13,44 @@ def save_user(message, admin=False):
 
 def get_user(message):
     user_id = message.from_user.id
-    return db.Users.hgetall(user_id)
+    user = db.Users.hgetall(user_id)
+    user['admin'] = user['admin'] == 'True'
+    return user
+
+
+def get_next_unique_id():
+    return db.Monitors.incr('id_counter')
+
+
+def save_monitor(brand=None, manufacture_year=None, price=None):
+    db.Monitors.hmset(get_next_unique_id(), {
+        'brand': brand,
+        'manufacture_year': manufacture_year,
+        'price': price,
+    })
+
+
+def readable_monitor_info(unique_id=None, brand=None,
+                          manufacture_year=None, price=None):
+    return """{}: Brand: {} Manufacture year: {} Price: {}\n""".format(
+        unique_id, brand, manufacture_year, price)
+
+
+def get_monitors():
+    result = ''
+    for key in db.Monitors.scan_iter():
+        if (key == 'id_counter'):
+            continue
+        values = db.Monitors.hgetall(key)
+        result += readable_monitor_info(
+            unique_id=key,
+            brand=values['brand'],
+            manufacture_year=values['manufacture_year'],
+            price=values['price'])
+    return result
+
+
+def remove_monitor(unique_id):
+    if (not db.Monitors.exists(unique_id)):
+        raise Exception('{} does not exist in the stock'.format(unique_id))
+    db.Monitors.delete(unique_id)
